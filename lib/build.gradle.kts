5fe1b4beafe4371a48eb36004b49a546a19bda1c
SMOLKEYS <jiroshimonis@gmail.com>
2022-12-11 11:15:23 +0800

    for those people

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -8,10 +8,10 @@
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.21"
 
     // Apply the java-library plugin for API and implementation separation.
-    `java-library`
+    java
 }
 
 repositories {
@@ -24,6 +24,27 @@ val mV = "v140"
 
 dependencies {
     compileOnly("com.github.Anuken.Arc:arc-core:$mV")
-    compileOnly("com.github.Anuken.Arc:flabel:$mV")
-    compileOnly("com.github.Anuken.MindustryJitpack:core:$mV")
+    //compileOnly("com.github.Anuken.Arc:flabel:$mV")
+    compileOnly("com.github.Anuken.Mindustry:core:$mV")
+}
+
+tasks.jar{
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+	manifest{
+		attributes["Main-Class"] = "com.github.smol.kotmindy.KotMindyKt"
+	}
+
+	from(*configurations.runtimeClasspath.files.map { if (it.isDirectory()) it else zipTree(it) }.toTypedArray())
+}
+
+tasks.register<Copy>("deploy"){
+	dependsOn("jar")
+
+	from("build/libs/lib.jar")
+	into("..")
+
+	doLast{
+		delete("build/libs/lib.jar")
+	}
 }
---
+++ lib/src/main/kotlin/com/github/smol/kotmindy/KotMindy.kt
@@ -0,0 +1,5 @@
+package com.github.smol.kotmindy
+
+fun main(args: Array<String>){
+	println("Kotmindy!")
+}
